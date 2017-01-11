70a88ea8644c649bc9845f3fedc4a7e36d8c5168
nathandm <nathan.damie@zenika.com>
2017-01-11 17:24:35 +0100

    travis: fix on local test on travis

--- front/test/awesome-tests.js
+++
@@ -1,8 +0,0 @@
-suite('<s-reactivity>', function () {
-    test('SHOULD BE TRUUUUe', function () {
-        assert.isNotNull({toto: 'toto'});
-    });
-    test('have navbar', function () {
-        assert.isNotNull(document.querySelector('s-reactivity').shadowRoot.querySelector('s-navbar'));
-    });
-});

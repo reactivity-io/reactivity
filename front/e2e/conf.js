8ac5af44a5a3e01dedc944be0ffdd2a6143bebac
nathandm <nathan.damie@zenika.com>
2017-01-13 14:13:34 +0100

    travis: start webdriver with dedicated conf

--- front/e2e/conf.js
+++ front/e2e/conf.js
@@ -3,8 +3,9 @@ const config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec.js']
 };
-
+console.log('travis process env : ', process.env.TRAVIS);
 if (process.env.TRAVIS) {
+    console.log("TRAVISSSSSSSS !!!!!!!")
     config.sauceUser = process.env.SAUCE_USERNAME;
     config.sauceKey = process.env.SAUCE_ACCESS_KEY;
     config.capabilities = {
@@ -12,6 +13,8 @@ if (process.env.TRAVIS) {
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER
     };
+    config.chromeOnly = true;
+    config.directConnect = true;
 }
 
 // conf.js

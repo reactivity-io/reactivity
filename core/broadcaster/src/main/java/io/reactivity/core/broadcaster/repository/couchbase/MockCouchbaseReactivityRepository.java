c4c3efd91da1e269e04fd85a35dce4450c73fdd3
gdrouet <guidrouet@gmail.com>
2017-01-17 19:15:32 +0100

    cosmetic: remove unused import

--- core/broadcaster/src/main/java/io/reactivity/core/broadcaster/repository/couchbase/MockCouchbaseReactivityRepository.java
+++ core/broadcaster/src/main/java/io/reactivity/core/broadcaster/repository/couchbase/MockCouchbaseReactivityRepository.java
@@ -27,7 +27,6 @@ import io.reactivity.core.lib.event.Organization;
 import io.reactivity.core.lib.event.Period;
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Repository;

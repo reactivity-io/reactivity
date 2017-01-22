46ca2f0050adcaaade908544dc07004ca2e42f6f
gdrouet <guidrouet@gmail.com>
2017-01-22 19:39:42 +0100

    deployment: push front module to CloudFoundry
    Statics are packaged inside a dist directory which is generated when the build has been successfully executed.
    Then, the Staticfile buildback is executed to upload the directory content to CloudFoundry.
    The deployed content is currently not optimized for production (no minification, caching, etc).
    Also note that all bower components (JS and HTML files) are copied without a module import resolution process.
    Currently the deployment should not really work as API URL can't be configured while is it provisionned dynamically by CloudFoudry.
    A future commit will fix it.

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -2,12 +2,28 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const proxyMiddleware = require('http-proxy-middleware');
 const historyApiFallback = require('connect-history-api-fallback');
+const fs = require('fs');
 
 const options = {
     target: 'http://localhost:8080',
     changeOrigin: true,
     pathRewrite: {
         '^/api': '/'
+    }
+};
+
+var deleteFolderRecursive = (path) => {
+    if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach((file,index) => {
+            var curPath = path + "/" + file;
+            if(fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+
+        fs.rmdirSync(path);
     }
 };
 
@@ -25,3 +41,26 @@ gulp.task('default', () => {
         }
     });
 });
+
+gulp.task('dist-clean', (done) => {
+    deleteFolderRecursive('./dist');
+	done();
+});
+
+gulp.task('dist-bower', (done) => {
+    gulp.src(['./bower_components/**/*.{js,html}'])
+        .pipe(gulp.dest("./dist/bower_components"));
+	done();
+});
+
+gulp.task('dist-src', (done) => {
+    gulp.src(['./src/**/*']).pipe(gulp.dest("./dist/src"));
+	done();
+});
+
+gulp.task('dist-index', (done) => {
+	gulp.src(['./index.html']).pipe(gulp.dest("./dist"));
+	done();
+});
+
+gulp.task('dist', gulp.series("dist-clean", "dist-bower", "dist-src", "dist-index"));

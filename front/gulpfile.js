08f1fedd5cc4982fd8e1dc7f7a1c431478690287
gdrouet <guidrouet@gmail.com>
2017-01-28 14:19:11 +0100

    config: exposes the broadcaster domain in a /api-domain.json endpoint
    Web application can now resolve the backend domain in a /api-domain.json endpoint.
    When deploying to CloudFoundry, gulp is used to generate this file before upload starts.
    The domain is retrieved from the manifest.yml where route is declared.
    For development, gulp handles this endpoint and specifies the localhost:3000/api domain.

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
+const file = require('gulp-file');;
 const browserSync = require('browser-sync').create();
 const proxyMiddleware = require('http-proxy-middleware');
 const historyApiFallback = require('connect-history-api-fallback');
 const fs = require('fs');
+const through = require('through2');
+const yaml = require('js-yaml');
 
 const options = {
     target: 'http://localhost:8080',
@@ -13,10 +16,11 @@ const options = {
 };
 
 var deleteFolderRecursive = (path) => {
-    if( fs.existsSync(path) ) {
+    if (fs.existsSync(path) ) {
         fs.readdirSync(path).forEach((file,index) => {
             var curPath = path + "/" + file;
-            if(fs.lstatSync(curPath).isDirectory()) { // recurse
+            
+            if( fs.lstatSync(curPath).isDirectory()) { // recurse
                 deleteFolderRecursive(curPath);
             } else { // delete file
                 fs.unlinkSync(curPath);
@@ -36,7 +40,13 @@ gulp.task('default', () => {
             index: 'index.html',
             middleware: [
                 historyApiFallback(),
-                proxyMiddleware('/api', options)
+                proxyMiddleware('/api', options),
+                {
+                    route: "/domain-api.json",
+                    handle: function (req, res, next) {
+                        res.end('["http://localhost:3000/api"]');
+                    }
+                }
             ]
         }
     });
@@ -44,23 +54,62 @@ gulp.task('default', () => {
 
 gulp.task('dist-clean', (done) => {
     deleteFolderRecursive('./dist');
-	done();
+    done();
 });
 
-gulp.task('dist-bower', (done) => {
+gulp.task('dist-bower', (done) => {    
     gulp.src(['./bower_components/**/*.{js,html}'])
         .pipe(gulp.dest("./dist/bower_components"));
-	done();
+    done();
 });
 
 gulp.task('dist-src', (done) => {
     gulp.src(['./src/**/*']).pipe(gulp.dest("./dist/src"));
-	done();
+    done();
 });
 
 gulp.task('dist-index', (done) => {
-	gulp.src(['./index.html']).pipe(gulp.dest("./dist"));
-	done();
+    gulp.src(['./index.html']).pipe(gulp.dest("./dist"));
+    done();
 });
 
-gulp.task('dist', gulp.series("dist-clean", "dist-bower", "dist-src", "dist-index"));
+gulp.task('staticfile', (done) => {
+    file('./dist/Staticfile', "", { src: true }).pipe(gulp.dest('./'));
+    done();
+});
+
+// This task expose in api-domain.json file the location of API endpoints to be consumed
+// This information is contained in the route attributes of manifest.yml, the CloudFoundry configuration file
+// The goal of this task is to parse the yaml file and extract the desired route into a file that will be uploaded on the HTTP server
+gulp.task('dist-api-domain', (done) => {
+    gulp.src(['../manifest.yml'])
+        .pipe(through.obj((chunk, enc, cb) => {
+            var doc = yaml.safeLoad(fs.readFileSync(chunk.path, 'utf8'));
+            var apiDomain = '[';
+            
+            doc.applications.every(application => {
+                if (application.name === 'reactivity-broadcaster') {
+                    application.routes.every((item, index) => {
+                        if (index > 0) {
+                            index += ", ";
+                        }
+                        
+                        apiDomain += "'" + item.route + "'";
+                        
+                        return true;
+                    });
+                    
+                    return false;
+                }
+                
+                return true;
+            });
+            
+            cb(null, apiDomain + ']');
+        }))
+        .pipe(fs.createWriteStream('./dist/api-domain.json'));
+        
+    done();
+});
+
+gulp.task('dist', gulp.series("dist-clean", "dist-bower", "dist-src", "dist-index", "staticfile", "dist-api-domain"));

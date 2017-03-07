86032a8b930d2596095abbb12a7783956d54ccf0
nathandm <nathan.damie@zenika.com>
2017-03-07 17:49:49 +0100

    api_domain: single call of api_domain

--- front/src/services/api_domain.js
+++ front/src/services/api_domain.js
@@ -1,8 +1,8 @@
+const deferred = Q.defer();
+let retval = null;
 const API_DOMAINS = () => {
     const req = new XMLHttpRequest();
-    const deferred = Q.defer();
     req.open('GET', '/domain-api.json', true);
-    let retval = null;
 
     req.onreadystatechange = () => {
         if (req.readyState == 4 && req.status == 200) {
@@ -12,7 +12,9 @@ const API_DOMAINS = () => {
     };
 
     try {
-        req.send(null);
+        if (!retval) {
+            req.send(null);
+        }
     } catch (e) {
         deferred.reject(e);
         console.log('%cDomain API is unavailable ! Let\'s start OFFLINE FIRST ?!', "font-size:1.5em;color:#4558c9;", "color:#d61a7f;font-size:1em;")
